a038042447aaf882755e95f3ad10877ddbb5ea5e
andyying <andy.ying@hotmail.com>
2019-12-25 21:43:29 +0100

    Added option to return to home

--- todo.js
+++ todo.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Escribe algo primero cara anchoa");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
